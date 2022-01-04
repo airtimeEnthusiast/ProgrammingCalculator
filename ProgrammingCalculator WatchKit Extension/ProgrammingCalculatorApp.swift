afff9c27974a5c999d4a79c201c9d471de28fab0
Austin Wright <9201assumplon9201@gmail.com>
2022-01-04 12:21:37 -0800

    Initial Commit

---
+++ ProgrammingCalculator WatchKit Extension/ProgrammingCalculatorApp.swift
@@ -0,0 +1,19 @@
+//
+//  ProgrammingCalculatorApp.swift
+//  ProgrammingCalculator WatchKit Extension
+//
+//  Created by Austin Wright on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ProgrammingCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
